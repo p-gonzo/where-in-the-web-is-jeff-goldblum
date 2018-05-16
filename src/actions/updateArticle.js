87c9e1237da15069d9c64255880c85f776165d6c
nickrzip <rogers.a.nick@gmail.com>
2018-05-15 23:06:48 -0400

    Late night commit - recursive fetch calls within a promise within a thunk @phil

--- src/actions/updateArticle.js
+++ src/actions/updateArticle.js
@@ -6,16 +6,37 @@ import changeArticleList from '../actions/articleList';
 var handleArticleChange = (query) => {
   //TODO: Learn what this code is doing
   return function thunk (dispatch) {
-    queryWikipedia(query).then(((data) => {
-      console.log(data);
+    queryWikipedia(query).then((data) => {
       // var newPage = data.query.normalized[0].to;
+
       var pageKey = Object.keys(data.query.pages)[0];
       var newLinks = data.query.pages[pageKey].links;
       newLinks = newLinks.map ((item) => item.title);
 
-      dispatch(changeArticleList(newLinks));
-      dispatch(changeArticle(query));
-    })); 
+      var addContinuedLinksRecursively = function (data, continueParams) {
+        queryWikipedia(query, continueParams).then((data) => {    
+          var continuedPageKey = Object.keys(data.query.pages)[0];
+          var continuedNewLinks = data.query.pages[continuedPageKey].links;
+          newLinks = newLinks.concat(continuedNewLinks.map ((item) => item.title));
+          if (data.continue) {
+            addContinuedLinksRecursively(query, data.continue.plcontinue)
+          } else {
+            dispatchData();
+          }
+        })
+      };
+
+      if (data.continue) {
+        addContinuedLinksRecursively(query, data.continue.plcontinue)
+      } else {
+        dispatchData();
+      }
+
+      function dispatchData() {
+        dispatch(changeArticleList(newLinks));
+        dispatch(changeArticle(query));
+      }
+    }); 
   };
 };
 
