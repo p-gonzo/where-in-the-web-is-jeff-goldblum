3a1faf018ce89e3c45ea3066a0831d995b437a24
Philip Gonzalez <pgonzalez115@gmail.com>
2018-05-17 20:47:32 -0400

    Refactor update Article Action

--- src/actions/updateArticle.js
+++ src/actions/updateArticle.js
@@ -4,38 +4,30 @@ import changeArticleList from '../actions/articleList';
 
 
 var handleArticleChange = (query) => {
-  //TODO: Learn what this code is doing
   return function thunk (dispatch) {
-    queryWikipedia(query).then((data) => {
+    var pageLinks = [];
 
-      var pageKey = Object.keys(data.query.pages)[0];
-      var newLinks = data.query.pages[pageKey].links;
-      newLinks = newLinks.map ((item) => item.title);
+    var addContinuedLinksRecursively = function (data, continueParams) {
+      
+      queryWikipedia(query, continueParams).then((data) => {    
+        
+        var pageKey = Object.keys(data.query.pages)[0];
+        var continuedLinks = data.query.pages[pageKey].links;
+        pageLinks = pageLinks.concat(continuedLinks.map ((item) => item.title));
+        
+        if (data.continue) {
+          addContinuedLinksRecursively(query, data.continue.plcontinue);
+        } else {
+          dispatchData();
+        }
+      });
+    };
 
-      var addContinuedLinksRecursively = function (data, continueParams) {
-        queryWikipedia(query, continueParams).then((data) => {    
-          var continuedPageKey = Object.keys(data.query.pages)[0];
-          var continuedNewLinks = data.query.pages[continuedPageKey].links;
-          newLinks = newLinks.concat(continuedNewLinks.map ((item) => item.title));
-          if (data.continue) {
-            addContinuedLinksRecursively(query, data.continue.plcontinue)
-          } else {
-            dispatchData();
-          }
-        })
-      };
-
-      if (data.continue) {
-        addContinuedLinksRecursively(query, data.continue.plcontinue)
-      } else {
-        dispatchData();
-      }
-
-      function dispatchData() {
-        dispatch(changeArticleList(newLinks));
-        dispatch(changeArticle(query));
-      }
-    }); 
+    function dispatchData() {
+      dispatch(changeArticleList(pageLinks));
+      dispatch(changeArticle(query));
+    }
+    addContinuedLinksRecursively(query);
   };
 };
 
